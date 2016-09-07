c58200d8361e962988ef9c2452b2b35d6df33adb
Garash2k <guillaumechabot1000@gmail.com>
2016-09-06 23:21:36 -0400

    Better spriggan spawn, encapsulated coord

--- script.js
+++ script.js
@@ -19,9 +19,10 @@
 
 	var spriggan = document.getElementById("spriggan-chase");	
 	spriggan.addEventListener("transitionend", doSprigganChase);
-	setTimeout(doSprigganChase, 10);
+	doSprigganChase();
 	
 	function setSprigganPos(sprigganPos, additionalTransform) {
+		additionalTransform = additionalTransform || "";
 		spriggan.style.transform = "translate(" + sprigganPos.x + "px, " + sprigganPos.y + "px)" + additionalTransform;
 	}
 	
@@ -32,60 +33,68 @@
 		
 		var durationMS = 4000 + 4000 * Math.random();
 		
+		var posInitialDesti = getPosInitialDesti();
+		
+		//Do the animation
+		spriggan.style.transition = "none";
+		setSprigganPos(posInitialDesti.Initial, posInitialDesti.AdditionalTransform);
+		
+		setTimeout(function() {
+			spriggan.style.transition = "transform " + durationMS + "ms linear";
+			setSprigganPos(posInitialDesti.Destination, posInitialDesti.AdditionalTransform);
+		}, 1);
+	}
+	
+	function getPosInitialDesti() {
+		var destinationLeft = 0 - SPRIGGAN_CHASE_WIDTH;
+		var destinationTop = 0 - SPRIGGAN_CHASE_HEIGHT;
+		
+		var posInitialDesti = {};
+		
 		//Set random pos+Orient by direction
 		var direction = Math.random() > 0.5 ? DIRECTION_HORIZONTAL : DIRECTION_VERTICAL
 		var isInvert = Math.random() > 0.5;
 		
-		var posInitial = {x: 0, y: 0};
-		var posDestination = {x: window.innerWidth, y: window.innerHeight};
+		posInitialDesti.Initial = {};
+		posInitialDesti.Destination = {};
 		
-		var additionalTransform = "";
+		posInitialDesti.AdditionalTransform = "";
 		if (direction === DIRECTION_HORIZONTAL) {
 			if (!isInvert) {
 				//x: 0->max
 				//y: random->random
-				posInitial.x = 0;
-				posDestination.x = window.innerWidth;
+				posInitialDesti.Initial.x = destinationLeft;
+				posInitialDesti.Destination.x = window.innerWidth;
 			} else {
 				//x: max->0
 				//y: random->random
-				posInitial.x = window.innerWidth;
-				posDestination.x = 0;
+				posInitialDesti.Initial.x = window.innerWidth;
+				posInitialDesti.Destination.x = destinationLeft;
 				
-				additionalTransform += " scaleX(-1) ";
+				posInitialDesti.AdditionalTransform += " scaleX(-1) ";
 			}
-			posInitial.y = Math.random() * window.innerHeight;
-			posDestination.y = Math.random() * window.innerHeight;
+			posInitialDesti.Initial.y = Math.random() * window.innerHeight;
+			posInitialDesti.Destination.y = Math.random() * window.innerHeight;
 		} else { //direction vertical
 			if (!isInvert) {
 				//x: random->random
 				//y: 0->max
-				posInitial.y = 0;
-				posDestination.y = window.innerHeight;
+				posInitialDesti.Initial.y = destinationTop;
+				posInitialDesti.Destination.y = window.innerHeight;
 			} else {
 				//x: random->random
 				//y: max->0
-				posInitial.y = window.innerHeight;
-				posDestination.y = 0;
+				posInitialDesti.Initial.y = window.innerHeight;
+				posInitialDesti.Destination.y = destinationTop;
 				
-				additionalTransform += " scaleY(-1) ";
+				posInitialDesti.AdditionalTransform += " scaleY(-1) ";
 			}
-			posInitial.x = Math.random() * window.innerWidth;
-			posDestination.x = Math.random() * window.innerWidth;
+			posInitialDesti.Initial.x = Math.random() * window.innerWidth;
+			posInitialDesti.Destination.x = Math.random() * window.innerWidth;
 			
-			additionalTransform += " rotate(90deg) ";
+			posInitialDesti.AdditionalTransform += " rotate(90deg) ";
 		}
 		
-		//Do the animation
-		posInitial.x = posInitial.x - SPRIGGAN_CHASE_WIDTH;
-		posInitial.y = posInitial.y - SPRIGGAN_CHASE_HEIGHT;
-		
-		spriggan.style.transition = "none";
-		setSprigganPos(posInitial, additionalTransform);
-		
-		setTimeout(function() {
-			spriggan.style.transition = "transform " + durationMS + "ms linear";
-			setSprigganPos(posDestination, additionalTransform);
-		}, 1);
+		return posInitialDesti;
 	}
 }) ();
