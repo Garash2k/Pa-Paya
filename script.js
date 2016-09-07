d7c54961131b57830e74c0fd233cc552643b9283
Garash2k <guillaumechabot1000@gmail.com>
2016-09-06 23:37:49 -0400

    spriggans are now clickable
    thanks ben for the idea

--- script.js
+++ script.js
@@ -17,84 +17,101 @@
 
 	audio.play();
 
-	var spriggan = document.getElementById("spriggan-chase");	
-	spriggan.addEventListener("transitionend", doSprigganChase);
-	doSprigganChase();
+	var initialSpriggan = document.getElementsByClassName("spriggan-chase")[0];
+	setTimeout(function() {
+		startSprigganChase(initialSpriggan);
+	}, 10);
 	
-	function setSprigganPos(sprigganPos, additionalTransform) {
-		additionalTransform = additionalTransform || "";
-		spriggan.style.transform = "translate(" + sprigganPos.x + "px, " + sprigganPos.y + "px)" + additionalTransform;
-	}
+	function startSprigganChase(spriggan) {
+		function setSprigganPos(sprigganPos, additionalTransform) {
+			additionalTransform = additionalTransform || "";
+			spriggan.style.transform = "translate(" + sprigganPos.x + "px, " + sprigganPos.y + "px)" + additionalTransform;
+		}
 	
-	function doSprigganChase() {
-		//Set random egg
-		var egg = document.getElementById("egg");
-		egg.src = "egg/egg-" + Math.ceil(Math.random() * NO_EGGS) + ".png";
-		
-		var durationMS = 4000 + 4000 * Math.random();
-		
-		var posInitialDesti = getPosInitialDesti();
-		
-		//Do the animation
-		spriggan.style.transition = "none";
-		setSprigganPos(posInitialDesti.Initial, posInitialDesti.AdditionalTransform);
-		
-		setTimeout(function() {
-			spriggan.style.transition = "transform " + durationMS + "ms linear";
-			setSprigganPos(posInitialDesti.Destination, posInitialDesti.AdditionalTransform);
-		}, 1);
-	}
-	
-	function getPosInitialDesti() {
-		var destinationLeft = 0 - SPRIGGAN_CHASE_WIDTH;
-		var destinationTop = 0 - SPRIGGAN_CHASE_HEIGHT;
-		
-		var posInitialDesti = {};
-		
-		//Set random pos+Orient by direction
-		var direction = Math.random() > 0.5 ? DIRECTION_HORIZONTAL : DIRECTION_VERTICAL
-		var isInvert = Math.random() > 0.5;
-		
-		posInitialDesti.Initial = {};
-		posInitialDesti.Destination = {};
-		
-		posInitialDesti.AdditionalTransform = "";
-		if (direction === DIRECTION_HORIZONTAL) {
-			if (!isInvert) {
-				//x: 0->max
-				//y: random->random
-				posInitialDesti.Initial.x = destinationLeft;
-				posInitialDesti.Destination.x = window.innerWidth;
-			} else {
-				//x: max->0
-				//y: random->random
-				posInitialDesti.Initial.x = window.innerWidth;
-				posInitialDesti.Destination.x = destinationLeft;
-				
-				posInitialDesti.AdditionalTransform += " scaleX(-1) ";
-			}
-			posInitialDesti.Initial.y = Math.random() * window.innerHeight;
-			posInitialDesti.Destination.y = Math.random() * window.innerHeight;
-		} else { //direction vertical
-			if (!isInvert) {
-				//x: random->random
-				//y: 0->max
-				posInitialDesti.Initial.y = destinationTop;
-				posInitialDesti.Destination.y = window.innerHeight;
-			} else {
-				//x: random->random
-				//y: max->0
-				posInitialDesti.Initial.y = window.innerHeight;
-				posInitialDesti.Destination.y = destinationTop;
-				
-				posInitialDesti.AdditionalTransform += " scaleY(-1) ";
-			}
-			posInitialDesti.Initial.x = Math.random() * window.innerWidth;
-			posInitialDesti.Destination.x = Math.random() * window.innerWidth;
+		function getPosInitialDesti() {
+			var destinationLeft = 0 - SPRIGGAN_CHASE_WIDTH;
+			var destinationTop = 0 - SPRIGGAN_CHASE_HEIGHT;
 			
-			posInitialDesti.AdditionalTransform += " rotate(90deg) ";
+			var posInitialDesti = {};
+			
+			//Set random pos+Orient by direction
+			var direction = Math.random() > 0.5 ? DIRECTION_HORIZONTAL : DIRECTION_VERTICAL
+			var isInvert = Math.random() > 0.5;
+			
+			posInitialDesti.Initial = {};
+			posInitialDesti.Destination = {};
+			
+			posInitialDesti.AdditionalTransform = "";
+			if (direction === DIRECTION_HORIZONTAL) {
+				if (!isInvert) {
+					//x: 0->max
+					//y: random->random
+					posInitialDesti.Initial.x = destinationLeft;
+					posInitialDesti.Destination.x = window.innerWidth;
+				} else {
+					//x: max->0
+					//y: random->random
+					posInitialDesti.Initial.x = window.innerWidth;
+					posInitialDesti.Destination.x = destinationLeft;
+					
+					posInitialDesti.AdditionalTransform += " scaleX(-1) ";
+				}
+				posInitialDesti.Initial.y = Math.random() * window.innerHeight;
+				posInitialDesti.Destination.y = Math.random() * window.innerHeight;
+			} else { //direction vertical
+				if (!isInvert) {
+					//x: random->random
+					//y: 0->max
+					posInitialDesti.Initial.y = destinationTop;
+					posInitialDesti.Destination.y = window.innerHeight;
+				} else {
+					//x: random->random
+					//y: max->0
+					posInitialDesti.Initial.y = window.innerHeight;
+					posInitialDesti.Destination.y = destinationTop;
+					
+					posInitialDesti.AdditionalTransform += " scaleY(-1) ";
+				}
+				posInitialDesti.Initial.x = Math.random() * window.innerWidth;
+				posInitialDesti.Destination.x = Math.random() * window.innerWidth;
+				
+				posInitialDesti.AdditionalTransform += " rotate(90deg) ";
+			}
+			
+			return posInitialDesti;
 		}
 		
-		return posInitialDesti;
+		function doSprigganChase() {
+		
+			//Set random egg
+			var egg = spriggan.getElementsByClassName("egg");
+			egg.src = "egg/egg-" + Math.ceil(Math.random() * NO_EGGS) + ".png";
+			
+			var durationMS = 4000 + 4000 * Math.random();
+			
+			var posInitialDesti = getPosInitialDesti();
+			
+			//Do the animation
+			spriggan.style.transition = "none";
+			setSprigganPos(posInitialDesti.Initial, posInitialDesti.AdditionalTransform);
+			
+			setTimeout(function() {
+				spriggan.style.transition = "transform " + durationMS + "ms linear";
+				setSprigganPos(posInitialDesti.Destination, posInitialDesti.AdditionalTransform);
+			}, 1);
+		}
+		
+		//Setup recursion.
+		spriggan.addEventListener("transitionend", doSprigganChase);
+		spriggan.addEventListener('click', createNewSpriggan);
+		
+		doSprigganChase();
+	}
+	
+	window.createNewSpriggan = createNewSpriggan;
+	function createNewSpriggan() {
+		var newSpriggan = initialSpriggan.cloneNode(true);
+		document.body.appendChild(newSpriggan);
+		startSprigganChase(newSpriggan);
 	}
 }) ();
